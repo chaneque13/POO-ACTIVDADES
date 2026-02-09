bf2fead6ee203aea5c2c4dc85e2474f2c647d9a1
Kayden <jveduig@gmail.com>
2026-02-09 16:31:55 -0600

    subido 1

---
+++ Actividad Practica 2/script.js
@@ -0,0 +1,13 @@
+function changePara(){
+    document.getElementById("parrafo").innerHTML = "<strong>JavaScript es un lenguaje de programación interpretado, dinámico y de alto nivel, fundamental para el desarrollo web moderno.  Aunque comparte similitudes sintácticas con Java, no está relacionado con este lenguaje y se utiliza principalmente para añadir interactividad a páginas web, permitiendo cambios dinámicos en el contenido, control de multimedia, animaciones y gestión de eventos en tiempo real. </strong>";
+
+}
+
+function changeBack(){
+    document.body.style.backgroundColor = "red";
+
+}
+
+function  messageEER(){
+    alert("ERROR 404");
+}
